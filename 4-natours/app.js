cefc16f38f8b48513ddb44003d9a3bf0e668f38a
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2024-05-03 15:34:17 +0300

    add Handel error

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -27,5 +29,10 @@ app.use((req, res, next) => {
 app.use('/api/v1/tour', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
 module.exports = app;
-// Start server
